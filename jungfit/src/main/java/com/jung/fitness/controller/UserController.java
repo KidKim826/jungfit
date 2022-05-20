3e528050f18ce648d81c39c5dbdb6835b108ad41
KidKim826 <moopama4@gmail.com>
2022-05-20 18:00:45 +0900

    second commit

--- jungfit/src/main/java/com/jung/fitness/controller/UserController.java
+++ jungfit/src/main/java/com/jung/fitness/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.jung.fitness.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class UserController {
 	
+	@PostMapping("/user")
+	public ResponseEntity<String> join() {
+		return new ResponseEntity<String>("SUCCESS", HttpStatus.ACCEPTED);
+	}
 }
